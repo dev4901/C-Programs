12c29d2adc98308868cc8eff4fece35a828e916f
Devansh Shah <dev4901@outlook.com>
2021-06-14 10:50:40 +0530

    insertion

--- sortings/insertion-sort/insertion.c
+++ sortings/insertion-sort/insertion.c
@@ -3,12 +3,14 @@
 int main()
 {
 	int a[7],i,j;
-	printf("enter the values of array - \n"); //enter 6 random values for array 
+	printf("enter the values of array - \n"); //enter 7 unsorted numbers
 	for(i=0;i<7;i++)
 	{
 		scanf("%d",&a[i]);
 	}
-	for (int i = 1; i < 7; i++)
+	
+	//method 1 - my method
+	/*for (int i = 1; i < 7; i++)
 	{
 		int key = a[i];
 		j=i-1;
@@ -17,8 +19,34 @@ int main()
 			a[j+1] = a[j]; a[j] = key;
 			j--;
 		}
+	}*/
+    
+    /* method 2 - book method
+    for (int i = 1; i < 7; i++)
+	{
+		int key = a[i];
+		j=i-1;
+		while(a[j]>key && j>=0)
+		{
+			a[j+1] = a[j]; 
+			j--;
+		}
+		a[j+1] = key;
+	}*/
+    
+    //for descending order
+    for (int i = 1; i < 7; i++)
+	{
+		int key = a[i];
+		j=i-1;
+		while(a[j]<key && j>=0)
+		{
+			a[j+1] = a[j]; 
+			j--;
+		}
+		a[j+1] = key;
 	}
-
+    
 	printf("\n");
 	for(i=0;i<7;i++)
 	{printf("%d , ",a[i]);}
